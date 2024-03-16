ef86e8497c29fcf6e765729d597efd9095e76abc
CVX63 <supachok8728@gmail.com>
2024-03-16 08:48:37 +0700

    update 1.1
    Change from redirect user to dish/id to show dish's detail in modal form

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,27 +1,60 @@
 <template>
   <div class="row">
     <div class="col">
-      <router-link
-        class="event-link"
-        :to="{ name: 'DishDetail', params: { id: event.RecipeId } }"
-      >
-        <div class="event-card">
-          <img
-            :src="event.Images[0]"
-            :alt="event.Name"
-            width="100"
-            height="100"
-          />
-          <h3>{{ event.Name }}</h3>
-        </div>
-      </router-link>
+      <dishModal :open="isOpen" @close="isOpen = !isOpen">
+        <img
+          :src="event.Images[0]"
+          :alt="event.Name"
+          width="300"
+          height="250"
+        />
+        <h1 class="head name">
+          {{ event.Name }}
+        </h1>
+        <hr />
+        <br />
+        <h2>Description</h2>
+        <p>{{ event.Description }}</p>
+        <hr />
+        <br />
+        <h2>List of ingredients</h2>
+        <ul v-for="list in event.RecipeIngredientParts" :key="list">
+          <li>► {{ list }}</li>
+        </ul>
+        <hr />
+        <br />
+        <h2>Cooking Processes</h2>
+        <ol v-for="list in event.RecipeInstructions" :key="list">
+          <li>►{{ list }}</li>
+        </ol>
+      </dishModal>
+      <div class="event-card">
+        <img
+          :src="event.Images[0]"
+          :alt="event.Name"
+          width="100"
+          height="100"
+        />
+        <h3>{{ event.Name }}</h3>
+        <button @click="isOpen = true">Show Modal</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import dishModal from "./modals/dishModal.vue";
+import { ref } from "vue";
 export default {
   name: "DishCard",
+  components: {
+    dishModal,
+  },
+  setup() {
+    const isOpen = ref(false);
+
+    return { isOpen };
+  },
   props: {
     event: {
       type: Object,
